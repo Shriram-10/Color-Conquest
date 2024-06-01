652bc75b4dedae9368675e69fd51d80b3214db2b
Shriram Umapathy <140259073+Shriram-10@users.noreply.github.com>
2024-06-01 17:57:54 +0530

    implement dark mode

--- app/src/main/java/com/example/task1colourconquest/HackerSettings.kt
+++ app/src/main/java/com/example/task1colourconquest/HackerSettings.kt
@@ -4,6 +4,7 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.slideIn
 import androidx.compose.animation.slideOut
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -24,9 +25,12 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -34,6 +38,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
@@ -681,15 +686,66 @@ fun HackerSettings(navController: NavController){
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ){
+        Button(
+            onClick = {},
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Transparent
+            ),
+            border = BorderStroke(2.dp, if (darkLight.value == 1) Color(126,219,225) else Color(49,119,120)),
+            shape = RoundedCornerShape(20),
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(0.9f),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "Dark Mode",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = if (darkLight.value == 1) Color.White else Color.Black
+                )
+                Spacer(modifier = Modifier.weight(1f))
+                Box {
+                    Switch(
+                        modifier = Modifier
+                            .height(80.dp)
+                            .width(80.dp)
+                            .scale(1.8f),
+                        checked = darkLight.value == 1,
+                        onCheckedChange = {
+                            darkLight.value = if (it) 1 else 0
+                        },
+                        colors = SwitchDefaults.colors(
+                            checkedThumbColor = Color(126, 219, 225),
+                            checkedTrackColor = Color(62, 83, 86),
+                            uncheckedThumbColor = Color(49, 119, 120),
+                            uncheckedTrackColor = Color(215, 234, 238),
+                            disabledCheckedThumbColor = Color.DarkGray,
+                            disabledCheckedTrackColor = Color.Black,
+                            disabledUncheckedThumbColor = Color.Yellow,
+                            disabledUncheckedTrackColor = Color.LightGray
+                        ),
+                        thumbContent = {}
+                    )
+                }
+            }
+        }
+        if (!isSeriesVisible && !isTimerVisible){
+            Spacer(modifier = Modifier.height(100.dp))
+        } else {
+            Spacer(modifier = Modifier.height(10.dp))
+        }
+
         AnimatedVisibility(
             visible = isSeriesVisible,
             enter = slideIn(
                 initialOffset = {fullSize -> IntOffset(fullSize.width, 0) },
-                animationSpec = tween(durationMillis = 1000)
+                animationSpec = tween(durationMillis = 200)
             ),
             exit = slideOut(
                 targetOffset = {fullSize -> IntOffset( -fullSize.width,0) },
-                animationSpec = tween(durationMillis = 1000)
+                animationSpec = tween(durationMillis = 200)
             ),
             modifier = Modifier
                 .fillMaxWidth(0.9f)
@@ -1263,7 +1319,7 @@ fun HackerSettings(navController: NavController){
                 if(isTimerVisible){
                     isTimerVisible = false
                     CoroutineScope(Dispatchers.Main).launch {
-                        delay(2000)
+                        delay(400)
                         isSeriesVisible = !isSeriesVisible
                     }
                 } else {
@@ -1295,7 +1351,7 @@ fun HackerSettings(navController: NavController){
                 if (isSeriesVisible){
                     isSeriesVisible = false
                     CoroutineScope(Dispatchers.Main).launch {
-                        delay(2000)
+                        delay(400)
                         isTimerVisible = !isTimerVisible
                     }
                 } else {
@@ -1325,11 +1381,11 @@ fun HackerSettings(navController: NavController){
             visible = isTimerVisible,
             enter = slideIn(
                 initialOffset = {fullSize -> IntOffset(fullSize.width, 0) },
-                animationSpec = tween(durationMillis = 1000)
+                animationSpec = tween(durationMillis = 200)
             ),
             exit = slideOut(
                 targetOffset = {fullSize -> IntOffset( -fullSize.width,0) },
-                animationSpec = tween(durationMillis = 1000)
+                animationSpec = tween(durationMillis = 200)
             ),
             modifier = Modifier
                 .fillMaxWidth(0.9f)
--- app/src/main/java/com/example/task1colourconquest/HomePage.kt
+++ app/src/main/java/com/example/task1colourconquest/HomePage.kt
@@ -8,6 +8,7 @@ import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -22,6 +23,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -31,6 +33,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
@@ -141,25 +144,7 @@ fun HomePage(navController: NavController/*, modifier: Modifier*/) {
         Image(painter = painterResource(id = if (darkLight.value == 1) R.drawable.darkplayers else R.drawable.players),
             contentDescription = "Players")
 
-        Spacer(modifier = Modifier.height(10.dp))
-
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.Center
-        ){
-            Switch(
-                modifier = Modifier
-                    .height(80.dp)
-                    .width(80.dp),
-                checked = darkLight.value == 1,
-                onCheckedChange = {
-                    darkLight.value = if (it) 1 else 0
-                },
-                thumbContent = {}
-            )
-        }
-
-        Spacer(modifier = Modifier.height(10.dp))
+        Spacer(modifier = Modifier.height(110.dp))
 
         Row {
             Button(
